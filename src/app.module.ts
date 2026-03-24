29da9aa5610b12e48ee92564a5be36942a013804
DavidChristopherNelson <david.nelson1@uqconnect.edu.au>
2026-03-24 11:35:11 -0700

    Fix minor error.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AcceptController } from './interfaces/whatsapp/inbound/accept/accept.controller';
-import { AcceptService } from './interfaces/whatsapp/inbound/accept/accept.service';
+import { AcceptController } from './interfaces/whatsapp/inbound/accept/accept.controller.js';
+import { AcceptService } from './interfaces/whatsapp/inbound/accept/accept.service.js';
 
 @Module({
   controllers: [AcceptController],
